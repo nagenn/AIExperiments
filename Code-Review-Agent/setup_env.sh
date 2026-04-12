a527cd9f6c7c52f982d95a0529e9603bf9f921c7
nagenn <44678680+nagenn@users.noreply.github.com>
2026-04-12 17:11:58 +0530

    Add files via upload

---
+++ Code-Review-Agent/setup_env.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#*******IMPORTANT - run this using source setup_env.sh************
+
+export OPENAI_API_KEY=<full value>
+export GITHUB_TOKEN=<full value>
+export GITHUB_REPO=nagenn/BooksManager
+export GITHUB_WEBHOOK_SECRET=<full value>
+
+echo "✅ Environment variables set"
